4f6547d4a9a08958fc77ad6d04853e2ead5bf3e1
Mauricio Lara <mau.lt7@hotmail.com>
2020-08-02 01:42:16 -0300

    Solucion de correcciones

--- Writer.c
+++ Writer.c
@@ -1,3 +1,13 @@
+/**
+ * @file    Writer.c
+ * @author  Jose Mauricio Lara Tapia (torneosmau@gmail.com)
+ * @brief   Comunicacion de procesos por FIFO
+ * @version 0.1
+ * @date    2020-08-02
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -9,97 +19,103 @@
 #include <stdint.h>
 #include <signal.h>
 
-#define FIFO_NAME "myfifo"          //Nombre de la fifo
-#define BUFFER_SIZE 300             //Tamano del buffer
-#define Header_Data "DATA"          //Cabezera del mensaje tipo dato
-#define Header_Sign "SIGN"          //Cabezera del mensaje tipo senal
+#define FIFO_NAME "myfifo" //Nombre de la fifo
+#define BUFFER_SIZE 300    //Tamano del buffer
+#define Header_Data "DATA" //Cabezera del mensaje tipo dato
+#define Header_Sign "SIGN" //Cabezera del mensaje tipo senal
 #define Size_Header_signal 6
-char msg_header[20];                //buffer para la cabezera del mensaje
-char outputBuffer[BUFFER_SIZE];    //buffer para envio del mensaje completo
+char msg_header[20];            //buffer para la cabezera del mensaje
+char outputBuffer[BUFFER_SIZE]; //buffer para envio del mensaje completo
 uint32_t bytesWrote;
 int32_t returnCode;
-int32_t fd;                         //File Descriptor
+int32_t fd; //File Descriptor
 
-void MySignalHandler1(int sig){
+void Handler(int sig)
+{
 
-    if ((bytesWrote = write(fd, "SIGN:1",Size_Header_signal )) == -1)
-            {
-			    perror("write");
-            }
-            else
-            {
-			    printf("writer: wrote %d bytes\n", bytesWrote);
+    if (sig == SIGUSR2)
+    {
+        if ((bytesWrote = write(fd, "SIGN:2", Size_Header_signal)) == -1)
+        {
+            perror("write");
         }
-
-}
-
-
-void MySignalHandler2(int sig){
-
-   if ((bytesWrote = write(fd, "SIGN:2", Size_Header_signal)) == -1)
-            {
-			    perror("write");
-            }
-            else
-            {
-			    printf("writer: wrote %d bytes\n", bytesWrote);
+        else
+        {
+            printf("writer: wrote %d bytes\n", bytesWrote);
         }
-
+    }
+    if (sig == SIGUSR1)
+    {
+        if ((bytesWrote = write(fd, "SIGN:1", Size_Header_signal)) == -1)
+        {
+            perror("write");
+        }
+        else
+        {
+            printf("writer: wrote %d bytes\n", bytesWrote);
+        }
+    }
 }
 
 int main(void)
 {
-   
-   
-  
+    struct sigaction sa;
+    sa.sa_handler = Handler;
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
     /* Create named fifo. -1 means already exists so no action if already exists */
-    if ( (returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0) ) < -1 )
+    if ((returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0)) < -1)
     {
         printf("Error creating named fifo: %d\n", returnCode);
         exit(1);
     }
 
     /* Open named fifo. Blocks until other process opens it */
-	printf("waiting for readers...\n");
-	if ( (fd = open(FIFO_NAME, O_WRONLY) ) < 0 )
+    printf("waiting for readers...\n");
+    if ((fd = open(FIFO_NAME, O_WRONLY)) < 0)
     {
         printf("Error opening named fifo file: %d\n", fd);
         exit(1);
     }
-    
+
     /* open syscalls returned without error -> other process attached to named fifo */
-	printf("got a reader--type some stuff\n");
+    printf("got a reader--type some stuff\n");
 
     /* Loop forever */
-
-    signal(SIGUSR1,&MySignalHandler1);
-    signal(SIGUSR2,&MySignalHandler2);
-    
-	while (1)
-	{
+    /*
+    signal(SIGUSR1, &MySignalHandler1);
+    signal(SIGUSR2, &MySignalHandler2);
+*/
+    while (1)
+    {
         /* Get some text from console */
-		fgets(outputBuffer, BUFFER_SIZE, stdin);
+        if (NULL != fgets(outputBuffer, BUFFER_SIZE, stdin))
+        {
+            sscanf(outputBuffer, "%[^:]:", msg_header);
 
-        sscanf(outputBuffer,"%[^:]:",msg_header);
-
-        if(strcmp(msg_header,Header_Data)!=0){
-
-            perror("mensaje incorrecto\n\n");
-
-        }
-        else{
-            /* Write buffer to named fifo. Strlen - 1 to avoid sending \n char */
-		    if ((bytesWrote = write(fd, outputBuffer, strlen(outputBuffer)-1)) == -1)
+            if (strcmp(msg_header, Header_Data) != 0)
             {
-			    perror("write");
+                perror("mensaje incorrecto\n\n");
             }
             else
             {
-			    printf("writer: wrote %d bytes\n", bytesWrote);
+                /* Write buffer to named fifo. Strlen - 1 to avoid sending \n char */
+                if ((bytesWrote = write(fd, outputBuffer, strlen(outputBuffer) - 1)) == -1)
+                {
+                    perror("write");
+                }
+                else
+                {
+                    printf("writer: wrote %d bytes\n", bytesWrote);
+                }
+            }
         }
-
+        else
+        {
+            perror("write");
         }
-        
-        	}
-	return 0;
+        close(fd);
+    }
+    return 0;
 }
